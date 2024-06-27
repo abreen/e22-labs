433ef853a8716bb3e6d5bdb9db8ed22c221dad3b
Alex Breen <github@abreen.email>
2024-06-27 00:44:47 -0400

    Lab 2 (#3)

---
+++ lab2-solution/src/test/java/StringRecursionTest.java
@@ -0,0 +1,58 @@
+import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
+
+public class StringRecursionTest {
+
+    @Test
+    public void testRemoveCapitals() {
+        assertEquals("def123 xy", StringRecursion.removeCapitals("ABCdef123 xyWZ"));
+        assertEquals("ooar", StringRecursion.removeCapitals("FooBar"));
+        assertEquals("zzz", StringRecursion.removeCapitals("AAAzzzAAA"));
+        assertEquals("s", StringRecursion.removeCapitals("Ms"));
+    }
+
+    @Test
+    public void testRemoveCapitalsEmptyOrNull() {
+        assertEquals("", StringRecursion.removeCapitals(""));
+
+        // testing that an exception will be thrown (using a lambda)
+        assertThrows(IllegalArgumentException.class,
+                () -> StringRecursion.removeCapitals(null));
+
+        // here's a simpler way using Assert.fail()
+        try {
+            StringRecursion.removeCapitals(null);
+            Assert.fail("");
+        } catch (IllegalArgumentException expected) {
+        }
+    }
+
+    @Test
+    public void testNumOccur() {
+        String str = "Mississippi";
+        assertEquals(4, StringRecursion.numOccur('s', str));
+        assertEquals(0, StringRecursion.numOccur('e', str));
+    }
+
+    @Test
+    public void testNumOccurEmptyOrNull() {
+        assertEquals(0, StringRecursion.numOccur('?', ""));
+        assertEquals(0, StringRecursion.numOccur('?', null));
+    }
+
+    @Test
+    public void testRemoveVowels() {
+        assertEquals("Msssspp", StringRecursion.removeVowels("Mississippi"));
+        assertEquals("", StringRecursion.removeVowels("ooo"));
+        assertEquals("fbr", StringRecursion.removeVowels("foobar"));
+    }
+
+    @Test
+    public void testRemoveVowelsEmptyOrNull() {
+        assertEquals("", StringRecursion.removeVowels(""));
+        assertNull(StringRecursion.removeVowels(null));
+    }
+}
