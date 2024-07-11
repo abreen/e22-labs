0cc3849a7d1fcd96af7bd49fe7e0b997065c5e82
Alex Breen <github@abreen.email>
2024-07-11 11:51:56 -0400

    Improve Lab 5 tests

--- lab5-solution/src/test/java/StringNodeTest.java
+++ lab5-solution/src/test/java/StringNodeTest.java
@@ -4,20 +4,21 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class StringNodeTest {
 
     @Test
     public void testConvert() {
         StringNode str = StringNode.convert("fine");
-        Assert.assertEquals("fine", str.toString());
+        assertEquals("fine", str.toString());
     }
 
     @Test
     public void testCharAt() {
         StringNode str = StringNode.convert("foobar");
-        Assert.assertEquals('f', StringNode.charAt(str, 0));
-        Assert.assertEquals('r', StringNode.charAt(str, 5));
+        assertEquals('f', StringNode.charAt(str, 0));
+        assertEquals('r', StringNode.charAt(str, 5));
 
         Assert.assertThrows(IllegalArgumentException.class, () -> {
             StringNode.charAt(str, 100);
@@ -25,26 +26,33 @@ public class StringNodeTest {
     }
 
     @Test
-    public void testUpperCase() {
-        StringNode str = StringNode.convert("fine");
-        StringNode.toUpperCase(str);
-        Assert.assertEquals("FINE", str.toString());
+    public void testEmptyLength() {
+        assertEquals(0, StringNode.length(null));
     }
 
     @Test
     public void testLength() {
         StringNode str = StringNode.convert("fine");
-        Assert.assertEquals(4, StringNode.length(str));
+        assertEquals(4, StringNode.length(str));
+    }
 
-        Assert.assertEquals(0, StringNode.length(null));
+    @Test
+    public void testDeleteFirstChar() {
+        StringNode str = StringNode.convert("cable");
+        str = StringNode.deleteChar(str, 0);
+
+        assertEquals('a', StringNode.charAt(str, 0));
+        assertEquals('b', StringNode.charAt(str, 1));
+        assertEquals('l', StringNode.charAt(str, 2));
+        assertEquals('e', StringNode.charAt(str, 3));
+        //assertEquals("able", str.toString());
     }
 
     @Test
     public void testDeleteChar() {
         StringNode str = StringNode.convert("summmer");
         str = StringNode.deleteChar(str, 2);
-
-        Assert.assertEquals("summer", str.toString());
+        assertEquals("summer", str.toString());
     }
 
     @Test
@@ -55,53 +63,58 @@ public class StringNodeTest {
         str2 = StringNode.deleteChar(str2, 0);
 
         // check that the original was not modified
-        Assert.assertEquals('o', StringNode.charAt(str1, 0));
+        assertEquals('o', StringNode.charAt(str1, 0));
 
-        Assert.assertEquals('r', StringNode.charAt(str2, 0));
+        assertEquals('r', StringNode.charAt(str2, 0));
     }
 
     @Test
     public void testInsertChar() {
         StringNode str = StringNode.convert("seam");
         str = StringNode.insertChar(str, 1, 't');
-
-        Assert.assertEquals("steam", str.toString());
+        assertEquals("steam", str.toString());
     }
 
     @Test
     public void testInsertSorted() {
         StringNode str = StringNode.convert("abcxyz");
         str = StringNode.insertSorted(str, 'm');
+        assertEquals("abcmxyz", str.toString());
+    }
 
-        Assert.assertEquals("abcmxyz", str.toString());
+    @Test
+    public void testUpperCase() {
+        StringNode str = StringNode.convert("fine");
+        StringNode.toUpperCase(str);
+        assertEquals("FINE", str.toString());
     }
 
     @Test
     public void testNumOccur() {
         StringNode str = StringNode.convert("hello");
-        Assert.assertEquals(2, StringNode.numOccur(str, 'l'));
-        Assert.assertEquals(0, StringNode.numOccur(str, 'z'));
+        assertEquals(2, StringNode.numOccur(str, 'l'));
+        assertEquals(0, StringNode.numOccur(str, 'z'));
     }
 
     @Test
     public void testNumOccurIterative() {
         StringNode str = StringNode.convert("hello");
-        Assert.assertEquals(2, StringNode.numOccurIterative(str, 'l'));
-        Assert.assertEquals(0, StringNode.numOccurIterative(str, 'z'));
+        assertEquals(2, StringNode.numOccurIterative(str, 'l'));
+        assertEquals(0, StringNode.numOccurIterative(str, 'z'));
     }
 
     @Test
     public void testRead() throws IOException {
         var input = getStreamForString("hello\n");
         StringNode str = StringNode.read(input);
-        Assert.assertEquals("hello", str.toString());
+        assertEquals("hello", str.toString());
     }
 
     @Test
     public void testReadIterative() throws IOException {
         var input = getStreamForString("hello\n");
         StringNode str = StringNode.readIterative(input);
-        Assert.assertEquals("hello", str.toString());
+        assertEquals("hello", str.toString());
     }
 
     private InputStream getStreamForString(String str) {
