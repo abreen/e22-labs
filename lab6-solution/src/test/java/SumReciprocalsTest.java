8f7f711eaefff0c8cb045feb7fd0a6e55d9cc4e7
Alex Breen <github@abreen.email>
2024-07-15 19:01:14 -0400

    Add test cases

--- lab6-solution/src/test/java/SumReciprocalsTest.java
+++ lab6-solution/src/test/java/SumReciprocalsTest.java
@@ -7,4 +7,11 @@ public class SumReciprocalsTest {
     public void testBaseCase() {
         Assert.assertEquals(0.0, SumReciprocals.sumReciprocals(0), 0.01);
     }
+
+    @Test
+    public void testSimpleCases() {
+        Assert.assertEquals(1.0, SumReciprocals.sumReciprocals(1), 0.01);
+        Assert.assertEquals(1.0 + (1.0 / 2), SumReciprocals.sumReciprocals(2), 0.01);
+        Assert.assertEquals(1.0 + (1.0 / 2) + (1.0 / 3), SumReciprocals.sumReciprocals(3), 0.01);
+    }
 }
